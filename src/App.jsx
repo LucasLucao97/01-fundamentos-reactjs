b7cd8a070acc4c930cb64d18b00de350709bb9a4
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-11-08 16:00:24 -0300

    Feat (Sidebar Component) Feature the sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { Header } from './components/Header'
+import { Header, Sidebar } from './components/Header'
 import { Post } from './Post'
-import './styles.css'
+
+import styles from './App.module.css'
+import './global.css'
 
 export function App() {
   return (
       <div>
         <Header />
-        
-        <Post 
-          author="Lucas Barbosa"
-          content=" 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Laborum pariatur perferendis quae ullam hic, 
-            sapiente repudiandae dolor. 
-            Architecto odit dolorum voluptas. 
-            Placeat sed quam cum sequi sit fugit corrupti porro?"
-        />
-        <Post
-          author="Gabriel Buzolin"
-          content="
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Sunt dolore quasi enim id veritatis, 
-            sequi incidunt culpa consectetur, nisi suscipit, 
-            sint fuga illum obcaecati corrupti rerum voluptate ipsa quidem nemo."
-        />
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            <Post 
+              author="Lucas Barbosa"
+              content=" 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Laborum pariatur perferendis quae ullam hic, 
+                sapiente repudiandae dolor. 
+                Architecto odit dolorum voluptas. 
+                Placeat sed quam cum sequi sit fugit corrupti porro?"
+            />
+            <Post
+              author="Gabriel Buzolin"
+              content="
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Sunt dolore quasi enim id veritatis, 
+                sequi incidunt culpa consectetur, nisi suscipit, 
+                sint fuga illum obcaecati corrupti rerum voluptate ipsa quidem nemo."
+            />
+          </main>
+        </div>
       </div>
   )
 }
