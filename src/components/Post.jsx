855c867f044dba84b68bfad1364661df9f248fc9
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-11-19 02:00:16 -0300

    feat :sparkles: (src) Feature comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,8 +14,25 @@ export function Post () {
         <time title='11 de Maio às 08:13:17' datetime="2022-05-11 08:13:17">Publicado há uma hora</time>
       </header>
       <div className={styles.content}>
-        <p>Oi</p>
+        <p>Fala galera</p>
+        <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da RocketSeat.</p>
+        <p><a href="https://bento.me/lucasbarbosa">Meu portifólio</a></p>
+        <p>
+          <p>#NovoProjeto</p>
+          <p>#Nlw</p>
+          <p>#RocketSeat</p>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
